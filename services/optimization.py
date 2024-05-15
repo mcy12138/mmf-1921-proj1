97d895d0605128f15bdfc81081a604f16e8c9882
J0hnnyZ <90335199+J0hnnyZ@users.noreply.github.com>
2024-05-14 22:24:32 -0400

    Update optimization.py

--- services/optimization.py
+++ services/optimization.py
@@ -44,5 +44,69 @@ def MVO(mu, Q):
     prob.solve(verbose=False)
     return x.value
 
+<<<<<<< Updated upstream
+=======
+def RP(Q):
+    n = Q.shape[0]
+
+    # Assign an arbitrary value for kappa
+    kappa = 5
+
+    # Define the optimization variable
+    x = cp.Variable(n)
+
+    # Objective function
+    objective = 0.5 * cp.quad_form(x, Q) - kappa * cp.sum(cp.log(x))
+
+    # Constraints
+    constraints = [x >= 0]  # x >= 0 and sum(x) = 1
+
+    # Define and solve the problem
+    problem = cp.Problem(cp.Minimize(objective), constraints)
+    problem.solve()
+
+    # Recover the weights
+    x_value = x.value/np.sum(x.value)
+
+    # Calculate the individual risk contribution per asset
+    RC = (x_value * (Q @ x_value)) / np.sqrt(np.dot(x_value.T, Q @ x_value))
+
+    # Return the optimized portfolio and the associated cost
+    return x_value
+
+
+def robustMVO(mu, Q, lambda_, alpha, T):
+    # Number of assets
+    n = Q.shape[0]
+
+    # Radius of the uncertainty set
+    ep = np.sqrt(chi2.ppf(alpha, n))
+
+    # Theta (squared standard error of expected returns)
+    Theta = np.diag(np.diag(Q)) / T
+
+    # Square root of Theta
+    sqrtTh = np.sqrt(Theta)
+
+    # Define the variable for the optimization
+    x = cp.Variable(n)
+
+    A = mu.T.to_numpy()
+
+    # Objective function
+    obj = cp.Minimize(lambda_ * cp.quad_form(x, Q) - A @ x + ep * cp.norm(sqrtTh @ x, 2))
+
+    # Constraints
+    constraints = [
+        cp.sum(x) == 1,
+        x >= 0
+    ]
+
+    # Solve the problem
+    prob = cp.Problem(obj, constraints)
+    prob.solve()
+
+    return x.value
+>>>>>>> Stashed changes
 
 
